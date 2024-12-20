032e7b349157d43534794058c1fc8fead6069e08
Lana-mm <malimonovas@list.ru>
2024-12-20 17:49:42 +0500

    end1

---
+++ test.py
@@ -0,0 +1,14 @@
+import pytest
+from main import count_vowels
+
+def test_only_vowels():
+    assert count_vowels("aeiouAEIOU") == 10
+
+def test_no_vowels():
+    assert count_vowels("bcdfghjklmnpqrstvwxyz") == 0
+
+def test_mixed_string():
+    assert count_vowels("Hello, World!") == 3
+    assert count_vowels("Python programming is fun!") == 6
+    assert count_vowels("AEIOUaeiou") == 10
+    assert count_vowels("12345!@#$%^&*()") == 0
